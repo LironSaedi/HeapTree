8c8fb0420d376bc1579e599e547f08872c2d81f3
Liron Saedi <Liron.Saedi@GMR.local>
2021-03-17 18:17:07 -0700

    I finished the heapify down

--- HeapTree/Heap.cs
+++ HeapTree/Heap.cs
@@ -4,11 +4,11 @@ using System.Text;
 
 namespace HeapTree
 {
-    class Heap<T>
+    class Heap<T> where T : IComparable<T>
     {
         public int Count = 0;
         T[] data = new T[10];
-        
+
         private void EnsureCapacity()
         {
             if (Count < data.Length)
@@ -24,7 +24,7 @@ namespace HeapTree
 
             data = tempData;
         }
-        
+
         public void Insert(T newItem)
         {
             EnsureCapacity();
@@ -32,22 +32,89 @@ namespace HeapTree
             data[Count] = newItem;
             Count++;
 
-            HeapifyUp(data);
+            HeapifyUp(Count - 1);
         }
-        private void HeapifyUp(T[] data)
+        private void HeapifyUp(int ind)
         {
-            //int indexOfParent;
+            int parent = (ind - 1) / 2;
+            //Here we get the parent by using the equation to convert it from array to the tree.
+
+            if (data[ind].CompareTo(data[parent]) < 0) // compare if the index is less than the parent. If it is swap the  index and the parent 0
+            {
+                T temporary = data[ind];
+                data[ind] = data[parent];
+                data[parent] = temporary;
+            }
+
+            HeapifyUp(parent); //Recursively call it and return the parent.
+        }
+
+        private void HeapifyDown(int index)
+        {
+            //Get left & Right
+            int leftIndex = index * 2 + 1;
+
+            //If no left then there will not be anything on the right.
+            if (leftIndex >= Count)
+            {
+                return;
+            }
+
+            int rightIndex = index * 2 + 2;
+            int swapIndex = 0;
+
+            //If there is no right child at this point, the smaller child is the left child
+            if (rightIndex >= Count)
+            {
+                swapIndex = leftIndex;
+            }
+
+            if (rightIndex > Count && leftIndex > Count)
+            {
+                if (data[rightIndex].CompareTo(data[leftIndex]) < 0)
+                {
+
+                    swapIndex = rightIndex;
+                    //T temporary = data[right];
+                    //data[right] = data[index];
+                    //data[index] = temporary;
+                    ////swapping the right
+                }
+                else
+                {
+                    swapIndex = leftIndex;
+                    //swap.E = data[left];
+                    //swapping the left
+                }
+
+            }
+            T temp = data[index];
+            data[index] = data[swapIndex];
+            data[swapIndex] = temp;
+            HeapifyDown(swapIndex);
+
 
         }
 
-        public void Pop()
+        public T Pop()
         {
 
+
+            //Return the first element of the tree
+            T root = data[0];
+
+            //Put last element as first eelemt.
+            data[0] = data[Count - 1];
+            //
+            //Update count
+            Count--;
+
+            //Heapify Down the last item that we put in the very front
+            HeapifyDown(0);
+
+            return root; //return the root
         }
 
-        private void HeapifyDown()
-        {
 
-        }
     }
 }
